bff99c533e3b7d56c1ebefec1ef8303dad7f2e2e
Pahan200-7 <pavlik.yakovlev.2000makron@mail.ru>
2025-10-03 08:36:39 +0300

    Были доделаны методы билдера спецификации для формирования пагинации курсором.

--- src/BulletinBoard/Application/BulletinBoard.AppServices/Contexts/Bulletin/Builders/CursorPaginationBuilders/BulletinMainCursorPaginationSpecificationBuilder.cs
+++ src/BulletinBoard/Application/BulletinBoard.AppServices/Contexts/Bulletin/Builders/CursorPaginationBuilders/BulletinMainCursorPaginationSpecificationBuilder.cs
@@ -19,26 +19,99 @@ public class BulletinMainCursorPaginationSpecificationBuilder : CursorPagination
     public IBulletinMainCursorPaginationSpecificationBuilder PaginateByTitle(Guid? lastId, string? lastTitle, bool ascending)
     {
 
-        //if (lastId.HasValue && !string.IsNullOrEmpty(lastTitle))
-        //{
-        //    if (ascending)
-        //    {
-        //        // Для ASC: ищем записи, которые идут ПОСЛЕ курсора
-        //        _specification.Add(b => b.Title > lastTitle || (b.Title == lastTitle && b.Id > lastId));
-        //    }
-        //    else
-        //    {
-        //        // Для DESC: ищем записи, которые идут ДО курсора
-        //        _specification.Add(b => b.Title < lastTitle || (b.Title == lastTitle && b.Id < lastId));
-        //    }
-        //}
+        if (lastId.HasValue && !string.IsNullOrEmpty(lastTitle))
+        {
+            if (ascending)
+            {
+                _specification.Add(b => b.Title.CompareTo(lastTitle) > 0 || (b.Title == lastTitle && b.Id > lastId));
+            }
+            else
+            {
+                _specification.Add(b => b.Title.CompareTo(lastTitle) < 0 || (b.Title == lastTitle && b.Id < lastId));
+            }
+        }
 
-        //// Важно: порядок сортировки должен соответствовать условию курсора
-        //Expression<Func<BulletinMain, object>> titleOrderByExpression = b => b.Title;
-        //_specification.AddOrderBy(titleOrderByExpression, ascending);
 
-        //Expression<Func<BulletinMain, object>> idOrderByExpression = b => b.Id;
-        //_specification.AddOrderBy(idOrderByExpression, ascending);
+        Expression<Func<BulletinMain, object>> titleOrderByExpression = b => b.Title;
+        _specification.AddOrderBy(titleOrderByExpression, ascending);
+
+        Expression<Func<BulletinMain, object>> idOrderByExpression = b => b.Id;
+        _specification.AddOrderBy(idOrderByExpression, ascending);
+        return this;
+    }
+
+    /// <inheritdoc/>
+    public IBulletinMainCursorPaginationSpecificationBuilder PaginateByDate(Guid? lastId, string? lastTitle, bool ascending)
+    {
+
+        if (lastId.HasValue && !string.IsNullOrEmpty(lastTitle))
+        {
+            if (ascending)
+            {
+                _specification.Add(b => b.Title.CompareTo(lastTitle) > 0 || (b.Title == lastTitle && b.Id > lastId));
+            }
+            else
+            {
+                _specification.Add(b => b.Title.CompareTo(lastTitle) < 0 || (b.Title == lastTitle && b.Id < lastId));
+            }
+        }
+
+
+        Expression<Func<BulletinMain, object>> titleOrderByExpression = b => b.Title;
+        _specification.AddOrderBy(titleOrderByExpression, ascending);
+
+        Expression<Func<BulletinMain, object>> idOrderByExpression = b => b.Id;
+        _specification.AddOrderBy(idOrderByExpression, ascending);
+        return this;
+    }
+
+    /// <inheritdoc/>
+    public IBulletinMainCursorPaginationSpecificationBuilder PaginateByDate(Guid? lastId, DateTime? lastDate, bool ascending)
+    {
+
+        if (lastId.HasValue && lastDate.HasValue)
+        {
+            if (ascending)
+            {
+                _specification.Add(b => b.CreatedAt > lastDate || (b.CreatedAt == lastDate && b.Id > lastId));
+            }
+            else
+            {
+                _specification.Add(b => b.CreatedAt < lastDate || (b.CreatedAt == lastDate && b.Id < lastId));
+            }
+        }
+
+
+        Expression<Func<BulletinMain, object>> dateOrderByExpression = b => b.CreatedAt;
+        _specification.AddOrderBy(dateOrderByExpression, ascending);
+
+        Expression<Func<BulletinMain, object>> idOrderByExpression = b => b.Id;
+        _specification.AddOrderBy(idOrderByExpression, ascending);
+        return this;
+    }
+
+    /// <inheritdoc/>
+    public IBulletinMainCursorPaginationSpecificationBuilder PaginateByPrice(Guid? lastId, decimal? lastPrice, bool ascending)
+    {
+
+        if (lastId.HasValue && lastPrice.HasValue)
+        {
+            if (ascending)
+            {
+                _specification.Add(b => b.Price > lastPrice || (b.Price == lastPrice && b.Id > lastId));
+            }
+            else
+            {
+                _specification.Add(b => b.Price < lastPrice || (b.Price == lastPrice && b.Id < lastId));
+            }
+        }
+
+
+        Expression<Func<BulletinMain, object>> priceOrderByExpression = b => b.Price;
+        _specification.AddOrderBy(priceOrderByExpression, ascending);
+
+        Expression<Func<BulletinMain, object>> idOrderByExpression = b => b.Id;
+        _specification.AddOrderBy(idOrderByExpression, ascending);
         return this;
     }
 }
